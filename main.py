06e24f8ab272f8cf56360852e08dab4d73153119
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2022-03-20 14:22:44 +0100

    Demo B&B

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 from graph import Node
 from branch_bound import Branch_and_Bound_Graph
 from exact import Exact_TSP_Graph
-
+import tsplib95 as tsp
+from numpy import sqrt
+from time import time
 class Graph(Branch_and_Bound_Graph, Exact_TSP_Graph):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
 def main():  
-    g = Graph(
-        nodes = [Node(label) for label in ['A', 'B', 'C', 'D']],
-        edges = {
-            ('A', 'B'): 1,
-            ('A', 'C'): 2,
-            ('A', 'D'): 3,
-            ('B', 'C'): 4,
-            ('B', 'D'): 5,
-            ('C', 'D'): 6
-        }
-    )
-    print(f"Exact solution: {g.exact_solution('A')}")
+    with open('test.tsp') as f:
+        lines = f.readlines()[8:]
+        node_coords = []
+        for line in lines:
+            _, x, y = [float(s) for s in line.split(' ')]
+            node_coords.append((x, y))
+        g = Graph(
+            nodes=[Node(chr(ord('A') + i)) for i in range(len(node_coords))],
+            edges={
+                (chr(ord('A') + i), chr(ord('A') + j)) : sqrt(
+                    (node_coords[i][0] - node_coords[j][0]) ** 2 + (node_coords[i][1] - node_coords[j][1]) ** 2
+                )
+                for i in range(len(node_coords)) for j in range(len(node_coords))
+            }
+        ) 
+    tic = time()
     print(f"B&B solution: {g.branch_and_bound('A')}")
+    toc = time()
+    print(f"temps: {toc - tic}")
+
+    tic = time()
+    print(f"Exact solution: {g.exact_solution('A')}")
+    toc = time()
+    print(f"temps: {toc - tic}")
+
 
 if __name__ == '__main__':
     main()
